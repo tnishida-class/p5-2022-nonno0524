2b7c7a98a8fdc70d7ee47c627e5e7bb14c62f982
nonno0524 <shibatyantatu@gmail.com>
2022-10-18 11:53:49 +0900

    Update section3-q1

--- section3-q1/sketch.js
+++ section3-q1/sketch.js
@@ -1,13 +1,15 @@
 // 練習問題「心臓の鼓動のように大きくなったり小さくなったりする円」
 let x;
 let y;
-let z;
+let count;
+let cycle;
 
 function setup(){
   createCanvas(windowWidth, windowHeight);
   x=width/2
   y=height/2
-  z=height
+  count=0;
+  cycle=100;
 }
 
 function windowResized(){
@@ -16,10 +18,12 @@ function windowResized(){
 
 function draw(){
   background(160, 192, 255);
+  count=(count+2)%cycle;
+  console.log(count);
   // BLANK[1]
-  ellipse(x, y, z);
-  z+=2
-  if(z>height){
-    z=-1*z
+  let size=count+100;
+  ellipse(x, y, size);
+  if(keyIsDown(" ".charCodeAt(0))){
+    count=(count+5)%cycle;
   }
-    }
+}
